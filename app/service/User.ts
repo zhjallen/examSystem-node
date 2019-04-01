00c0af961eee9e1669b783af02ffc2c8983f378e
zhjallen <1098414377@qq.com>
2019-04-01 18:04:43 +0800

    init

---
+++ app/service/User.ts
@@ -0,0 +1,19 @@
+import { Service } from 'egg';
+
+/**
+ * Test Service
+ */
+export default class Test extends Service {
+
+    /**
+     * sayHi to you
+     * @param name - your name
+     */
+    public async users() {
+        const users=await this.app.mysql.select("user");
+        return users;
+        
+    }
+
+
+}
