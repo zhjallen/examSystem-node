786ac3ad3cb7d112f3f0e4aab7137a1aca95cb16
zhjallen <1098414377@qq.com>
2019-05-11 17:32:24 +0800

    开发

--- app/model/user.ts
+++ app/model/user.ts
@@ -5,13 +5,19 @@ module.exports = app => {
 
   const User = app.model.define('user', {
     id: { type: INTEGER, primaryKey: true, autoIncrement: true },
-    name: STRING(30),
-    username:STRING(30),
+    name: {
+      type: STRING,
+      unique: true,
+    },
+    username: {
+      type: STRING,
+      unique: true,
+    },
     password: STRING(30),
     status: INTEGER, // 用户状态（1 启用,0 禁用）
     type: INTEGER, // 用户类型（1 管理员，2 学生）
     // age: INTEGER,
-    desc:STRING(255), // 描述
+    desc: STRING(255), // 描述
     created_at: DATE,
     updated_at: DATE,
   });
